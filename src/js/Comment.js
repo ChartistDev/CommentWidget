e6288ba33af8729a165af2f1f9e052cb9404a0d6
Debanjan Dey <db4friends@gmail.com>
2020-04-18 23:24:52 +0530

    rply and del functionality started

--- src/js/Comment.js
+++ src/js/Comment.js
@@ -1,14 +1,5 @@
-
-const post = (area, listDiv) => {
-   let commentJSON = createCommentJSON(area.value),
-   label;
-   if(commentJSON) {
-    label = document.createElement("label");
-    listDiv.appendChild(label);
-    label.innerHTML = commentJSON.comment;
-   }
-
-}
+const UNDEF = undefined;
+const state = [];
 const debounce = function(fn, delay){
     let timeout;
     return function() {
@@ -21,15 +12,57 @@ const debounce = function(fn, delay){
     }
 
 }
+const deleteComment = (el) => {
+    el.target.parentElement.parentElement.parentElement.remove();
+}
+const rplyComment = (el) => {
+    
+}
+const del = debounce(deleteComment, 200);
+const post = (area, listDiv) => {
+   let commentJSON = createCommentJSON(area.value),
+   label,div, rplybtn, delbtn, rplyText, delText,outerDiv,btnDiv;
+   if(commentJSON) {
+    div = document.createElement("div");
+    outerDiv = document.createElement("div");
+    btnDiv = document.createElement("div");
+    btnDiv.className = "btnDiv";
+    div.className = "comment_box";
+    div.appendChild(outerDiv);
+    label = document.createElement("textarea");
+    label.setAttribute("disabled" ,true);
+    rplybtn = document.createElement("button");
+    rplyText = document.createTextNode("REPLY");
+    rplybtn.appendChild(rplyText)
+    delbtn = document.createElement("button");
+    delText = document.createTextNode("DEL");
+    delbtn.appendChild(delText);
+    btnDiv.appendChild(rplybtn);
+    btnDiv.appendChild(delbtn);
+    label.setAttribute("rows", "4");
+    label.setAttribute("cols", "50");
+    outerDiv.appendChild(label);
+    outerDiv.appendChild(btnDiv);
+    listDiv.appendChild(div);
+    label.innerHTML = commentJSON.comment;
+
+    delbtn.addEventListener("click", del);
+    rplybtn.addEventListener("click", rply);
+
+   }
+
+}
 const createCommentJSON = (text) => {
-    let cmt = {}
+    let cmt = {};
     if(text && text !== "") {
         Object.assign(cmt, {
             comment: text,
             replies:[]
-        })
+        });
+        state.push(cmt);
+        return cmt;
     } 
-    return cmt;
+    return UNDEF;
 }
 const postComment = debounce(post, 300);
 export default postComment;
